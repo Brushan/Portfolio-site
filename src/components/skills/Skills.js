656ecfbefccefe2d063307113ffb31d1b350e5e3
brushan <tommy.berggrens@gmail.com>
2020-11-12 19:46:48 +0100

    Added SEO and analytics tech stack

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CssIcon, HtmlIcon, JsIcon, NodeIcon, ReactIcon, ReduxIcon, SassIcon, TypescriptIcon } from '../../media/icons/icons';
+import { CssIcon, GaIcon, GscIcon, GtmIcon, HtmlIcon, JsIcon, NodeIcon, ReactIcon, ReduxIcon, SassIcon, TypescriptIcon } from '../../media/icons/icons';
 import './Skills.scss';
 
 const Skills = () => {
@@ -7,7 +7,7 @@ const Skills = () => {
     <section className="skills">
       <div className="skillsWrapper">
         <div className="skillsWrapper__card">
-          <h3>Web dev</h3>
+          <h3>Web Development</h3>
           <div className="skillsWrapper__card__techGroup">
             <div className="tech">
               <HtmlIcon />
@@ -43,6 +43,20 @@ const Skills = () => {
             </div>
           </div>
           <h3>SEO & Web Analysis</h3>
+          <div className="skillsWrapper__card__techGroup">
+            <div className="tech">
+              <GaIcon />
+              <p>Analytics</p>
+            </div>
+            <div className="tech">
+              <GtmIcon />
+              <p>GTM</p>
+            </div>
+            <div className="tech">
+              <GscIcon />
+              <p>GSC</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
