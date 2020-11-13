0de2664cf6a4ccf2b899e213402b52fd66b85cf6
brushan <tommy.berggrens@gmail.com>
2020-11-13 18:16:59 +0100

    Added looping video to project

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Project.scss';
+import video from '../../media/videos/Hangman.mp4'
 
 export const Project = () => {
   return (
       <div className="projectWrapper">
         <div className="projectWrapper__project">
           <h5>Web Development</h5>
+          <video autoPlay muted loop src={video} type="video/mp4"/>
           <h3>Hangman game</h3>
-          <p>This is a React web app I made during a hackday - The classic pen and paper game Hangman.</p>
+          <p>This is a React web app of the classic pen and paper game Hangman which I made during a hackday.</p>
+          <ul>
+            <li>Javscript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
         </div>
       </div>
   )
